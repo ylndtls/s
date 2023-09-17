057ded6122d1d62c1c907462455097c4016a0913
ylndtls <144607459+ylndtls@users.noreply.github.com>
2023-09-17 19:53:39 +0800

    Add files via upload

--- exercise.cpp
+++ exercise.cpp
@@ -4,14 +4,14 @@ int main()
 {
 	int x;
 	double y;
-	int z;
+	double z;
 	double a;
 	printf("请输入你的体重（kg）：\n");
 	scanf("%d",&x);
 	printf("请输入你的身高(m)：\n");
 	scanf("%lf",&y);
-	z = y*y;
-	a = 1.0*x/z;
+	z = (double)y*y;
+	a = (double)x/z;
 	printf("你的bmi为：%lf",a);
 	system("pause");
 
