b3bdb78e5f60fb95d16fa02b9e2032f473dd3dd0
ylndtls <144607459+ylndtls@users.noreply.github.com>
2023-09-17 19:33:33 +0800

    Add files via upload
    这个程序会帮助计算你的bmi

---
+++ exercise.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+	double y;
+	int z;
+	double a;
+	printf("请输入你的体重（kg）：\n");
+	scanf("%d",&x);
+	printf("请输入你的身高(m)：\n");
+	scanf("%lf",&y);
+	z = y*y;
+	a = 1.0*x/z;
+	printf("你的bmi为：%lf",a);
+
+
+	return 0;
+}
